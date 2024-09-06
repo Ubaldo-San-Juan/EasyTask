ed6139c4568c48debb6aa580c2ba89190fd78d67
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-09-06 12:26:22 -0600

    Task list created in Angular

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { TaskService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
+  private tasksService = inject(TaskService)
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.tasksService.addTask({
+      title: this.enteredTitle(),
+      summary: this.enteredSummary(),
+      date: this.enteredDate(),
+    },
+      this.userId
+    );
+    this.close.emit();
+  }
+}
